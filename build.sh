529dfd14350b5e84153c21843bed7e92804f749d
lkl <18373293@buaa.edu.cn>
2022-11-28 14:42:58 +0800

    rm abs path in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 codec.py ./ ./
 mkdir -p build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=`pwd`/../install .. -DDyninst_DIR=/root/spack/opt/spack/linux-ubuntu20.04-aarch64/gcc-9.4.0/dyninst-12.2.0-srzw622srfw6wz5raojdirtbsqdd4k7l/lib/cmake/Dyninst/
+cmake -DCMAKE_INSTALL_PREFIX=`pwd`/../install .. -DDyninst_DIR=${Dyninst_DIR}
 make -j
 make install
 
